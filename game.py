61681f9a27b3da1bd56e0acc758309161588954b
programmer325-jetlearner <asyedtanim1@gmail.com>
2025-05-25 11:43:19 +0100

    Add files via upload

--- game.py
+++ game.py
@@ -21,7 +21,6 @@ def create_satellites():
     start_time=time()
 
 def draw():
-
     global total_time
     screen.blit("bg",(0,0))
     num=1
@@ -30,19 +29,31 @@ def draw():
         screen.draw.text(str(num),(satellite.pos[0],satellite.pos[1]+20))
         num+=1
     for line in lines:
-        screen.draw.line(line[0],line[1],"white")
-
+        screen.draw.line(line[0],line[1],(255,255,255))
     if current_satellite<number_of_satellites:
         total_time=time()-start_time
         screen.draw.text(str(round(total_time,1)),(10,10),fontsize=30)
     else:
         screen.draw.text(str(round(total_time,1)),(10,10),fontsize=30)
 
-    
+
 
 def update():
     
     pass
+
+def on_mouse_down(pos):
+    global current_satellite, lines
+    if current_satellite<number_of_satellites:
+        if satellites[current_satellite].collidepoint(pos):
+            if current_satellite>0:
+                lines.append((satellites[current_satellite-1].pos,satellites[current_satellite].pos))
+            current_satellite+=1
+        else:
+            lines=[]
+            current_satellite=0
+        
+
 
 create_satellites()
 
